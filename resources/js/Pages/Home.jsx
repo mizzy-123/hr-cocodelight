f37bfd9964122e350d9ae0bff0f2c44709c8dfd3
colondum <decarabian21@gmail.com>
2024-07-04 19:29:13 +0700

    setup project laravel inertia/vite/react

---
+++ resources/js/Pages/Home.jsx
@@ -0,0 +1,8 @@
+export default function Home() {
+    return (
+        <div>
+            <h1>Hello World</h1>
+            <a href={route("test")}>Test</a>
+        </div>
+    );
+}
---
+++ resources/js/app.jsx
@@ -0,0 +1,18 @@
+import "./bootstrap";
+
+import { createRoot } from "react-dom/client";
+import { createInertiaApp } from "@inertiajs/react";
+import { Ziggy } from "./ziggy";
+import { route } from "ziggy-js";
+
+createInertiaApp({
+    resolve: (name) => {
+        const pages = import.meta.glob("./Pages/**/*.jsx", { eager: true });
+        return pages[`./Pages/${name}.jsx`];
+    },
+    setup({ el, App, props }) {
+        window.route = (name, params, absolute) =>
+            route(name, params, absolute, Ziggy);
+        createRoot(el).render(<App {...props} />);
+    },
+});
